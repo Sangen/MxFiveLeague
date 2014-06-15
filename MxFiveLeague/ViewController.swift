255697afed2413f75e9e1d7bac532a0d6a9bf385
Kaoru Kawakita <sangendokei@gmail.com>
2014-06-16 00:40:27 +0900

    Next animation

--- MxFiveLeague/ViewController.swift
+++ MxFiveLeague/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet var canvas : UIImageView
     @IBOutlet var nextButton : UIButton
+    @IBOutlet var finishButton : UIButton
     @IBOutlet var clearButton : UIButton
+    @IBOutlet var charNumerLabel : UILabel
 
+    var charNumber      = 1
+    var maxCharNumber   = 4
+    
     var bezierPath    = UIBezierPath?()
     var undoStack     = UIBezierPath[]()
     var redoStack     = UIBezierPath[]()
@@ -24,6 +29,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.updateCharNumber()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,7 +52,7 @@ class ViewController: UIViewController {
         // initialize path
         self.bezierPath = UIBezierPath()
         self.bezierPath!.lineCapStyle = kCGLineCapRound
-        self.bezierPath!.lineWidth = 4
+        self.bezierPath!.lineWidth = 12
         self.bezierPath!.moveToPoint(currentPoint)
     }
     
@@ -69,7 +79,7 @@ class ViewController: UIViewController {
         // generate hidden drawspace
         UIGraphicsBeginImageContext(self.canvas.frame.size)
         self.lastDrawImage?.drawAtPoint(CGPointZero)
-        UIColor.lightGrayColor().setStroke()
+        UIColor.whiteColor().setStroke()
         path.stroke()
         self.canvas.image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
@@ -84,34 +94,37 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didPressNextButton(sender : UIButton) {
-        UIImageWriteToSavedPhotosAlbum(self.canvas.image, self, nil, nil)
+//        UIImageWriteToSavedPhotosAlbum(self.canvas.image, self, nil, nil)
         
         self.pagePathHistory.append(self.undoStack)
+        self.showAndHideCopyCanvas(self.canvas)
         self.didPressClearButton(self.clearButton)
+        self.charNumber++
+        self.updateCharNumber()
     }
     
-//     画像を保存する
-//    func save {
-//    //保存する画像を指定
-//    UIImage *image = [UIImage imageNamed:_imageName];
-//    //画像保存完了時のセレクタ指定
-//    SEL selector = @selector(onCompleteCapture:didFinishSavingWithError:contextInfo:);
-//    //画像を保存する
-//    UIImageWriteToSavedPhotosAlbum(image, self, selector, NULL);
-//    }
-//    
-//    //画像保存完了時のセレクタ
-//    - (void)onCompleteCapture:(UIImage *)screenImage
-//    didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
-//    {
-//    NSString *message = @"画像を保存しました";
-//    if (error) message = @"画像の保存に失敗しました";
-//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @""
-//    message: message
-//    delegate: nil
-//    cancelButtonTitle: @"OK"
-//    otherButtonTitles: nil];
-//    [alert show];
-//    }
+    @IBAction func didPressFinishButton(sender : UIButton) {
+        NSLog("Finish")
+    }
+    
+    func showAndHideCopyCanvas(canvas: UIImageView) {
+        var cloneCanvas = UIImageView(image: canvas.image)
+        self.canvas.addSubview(cloneCanvas)
+        UIView.animateWithDuration(0.3, animations: {() -> Void in
+                cloneCanvas.frame.origin.x = -cloneCanvas.frame.width
+            }, completion: {(Bool) -> Void in
+                cloneCanvas.removeFromSuperview()
+                cloneCanvas.image = nil
+            })
+    }
+    
+    func updateCharNumber() {
+        self.charNumerLabel.text = "\(self.charNumber)文字目"
+        if self.charNumber >= self.maxCharNumber {
+            self.nextButton.hidden = true
+            self.finishButton.hidden = false
+        }
+    }
+    
 }
 
